5ddb9b601734caaffe1580e7288643492c34a538
jamwindgasse <jorge_andres.marquez-windgasse@L.de>
2021-04-28 11:24:33 +0000

    Added Dockerfile

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,8 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://13.92.60.189:3001";
+
 
 
 function getSessions(cb) {
